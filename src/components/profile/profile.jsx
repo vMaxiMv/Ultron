5031ab954600c03507ee992ca9e65ce08c537b46
Maxim <maximvasiliyev314@gmail.com>
2023-06-30 20:31:03 +0300

    logout

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Form from "../form/form";
+import axios from "axios";
 
 function Profile(props) {
+    const [formData, setFormData] = useState({ username: 'user123'});
+    const handleLogout = async () =>{
+        try{
+            const response = await axios.post('http://localhost:5000/api/login', formData)
+
+    }
+    catch (error){
+        console.error(error)}
+    }
     return (
         <div>
             <h1>Ваш профиль</h1>
-            <button>Выйти</button>
+            <button onClick={handleLogout}>Выйти</button>
         </div>
     );
 }
