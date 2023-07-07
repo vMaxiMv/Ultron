1b62727036415508fbf102b35ec821076ebddb8e
AkWinchester <arsenijkiselev03@gmail.com>
2023-07-07 15:14:42 +0300

    перенаправление на страницу вход\регистрация

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,13 +1,19 @@
 import React, {useState} from 'react';
 import Form from "../form/form";
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
+
 axios.defaults.withCredentials = true;
+
 function Profile(props) {
+    const navigate = useNavigate()
 
     const handleLogout = async () =>{
         try{
             const response = await axios.post('http://localhost:5000/api/logout')
+            const redirectUrl = response.data['redirect_url']
 
+            navigate(redirectUrl)
         }
     catch (error){
         console.error(error)}
