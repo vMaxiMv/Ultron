096e59e82d1cbc08aa8f62335a00e872b09162c3
AkWinchester <arsenijkiselev03@gmail.com>
2023-07-16 18:16:31 +0300

    first version chart

--- src/components/charts/CommonCharts.jsx
+++ src/components/charts/CommonCharts.jsx
@@ -1,21 +1,66 @@
 import React, {useState} from 'react';
 import BarCharts from "./barCharts/BarCharts";
 import {UserData} from "../../data/Data";
+//////////////////////////////
+function transformData(data) {
 
+  const result = {};
+
+  // Находим максимальный год
+  const maxYear = Math.max(...data.map(item => item.date));
+
+  data.forEach(item => {
+    if (!result[item.id]) {
+      // Создаем массив по умолчанию нужной длины
+      result[item.id] = new Array(maxYear - 9).fill(0);
+    }
+
+    // Заполняем массив данными
+    const index = item.date - 10;
+    result[item.id][index] = item.pullups;
+  });
+
+  return result;
+
+}
+
+
+function getSortedDates(data) {
+
+  // Извлекаем все значения date
+  const dates = data.map(item => item.date);
+
+  // Удаляем дубликаты и сортируем
+  const uniqueSortedDates = [...new Set(dates)].sort((a, b) => a - b);
+
+  return uniqueSortedDates;
+
+}
+
+/////////////////////////////
 function CommonCharts(props) {
+    const formattedData = transformData(UserData);
+
     const [userData, setUserData] = useState({
-        labels: UserData.map((data) => data.year) ,
+        labels: getSortedDates(UserData) ,
         datasets:[{
             label: "Max",
-            data: UserData.map((data)=>data.pullups),
+            data: formattedData[1],
             backgroundColor: 'rgba(255,2,32,1)', // Цвет заполнения столбцов графика
             borderColor: 'rgb(0,0,0)', // Цвет границы столбцов графика
             borderWidth: 1,
         },
             {
                 label: "Arsen",
-        data: UserData.map((data)=>data.pullups),
+        data: formattedData[2],
         backgroundColor: 'rgba(65,255,2,1)', // Цвет заполнения столбцов графика
+        borderColor: 'rgb(0,0,0)', // Цвет границы столбцов графика
+        borderWidth: 1,
+},
+            {
+                label: "Lexa",
+        data: formattedData[3],
+        backgroundColor: 'rgb(2,15,255)', // Цвет заполнения столбцов графика
         borderColor: 'rgb(0,0,0)', // Цвет границы столбцов графика
         borderWidth: 1,
 },
@@ -30,6 +75,7 @@ function CommonCharts(props) {
         plugins: {
             legend: {
                 labels: {
+                    render: 'value',
                     color: 'black' // Изменение цвета шрифта на черный
                 }
             }
--- src/data/Data.js
+++ src/data/Data.js
@@ -3,13 +3,61 @@ export const UserData = [
         id:1,
         name: 'Maxim',
         pullups: 100,
-        year: 2023
+        date: 10
+    },
+        {
+        id:1,
+        name: 'Maxim',
+        pullups: 80,
+        date: 11
     },
     {
         id:2,
-        name: 'Arseniy',
-        pullups:50,
-        year: 2022
-    }
+        name: 'Arsen',
+        pullups: 90,
+        date: 11
+    },
+    {
+        id:1,
+        name: 'Maxim',
+        pullups: 120,
+        date: 12
+    },
+    {
+        id:1,
+        name: 'Maxim',
+        pullups: 140,
+        date: 13
+    },
+        {
+        id:2,
+        name: 'Arsen',
+        pullups: 100,
+        date: 14
+    },
+        {
+        id:1,
+        name: 'Maxim',
+        pullups: 80,
+        date: 15
+    },
+    {
+        id:1,
+        name: 'Maxim',
+        pullups: 120,
+        date: 16
+    },
+    {
+        id:1,
+        name: 'Maxim',
+        pullups: 140,
+        date: 17
+    },
+            {
+        id:3,
+        name: 'Lexa',
+        pullups: 10,
+        date: 11
+    },
 
 ]
