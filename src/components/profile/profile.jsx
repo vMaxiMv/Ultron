1f5e9b3ebb2d45e95032120f1bc4e276caa37157
AkWinchester <arsenijkiselev03@gmail.com>
2023-07-20 16:43:48 +0300

    test without server

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -25,6 +25,7 @@ function Profile(props) {
         fetchData();
     }, []);
 
+
     const handleLogout = async () => {
         try {
             const response = await axios.post('http://localhost:5000/api/logout');
