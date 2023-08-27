4f77139fd8f4ed283742e898596967065dd0ed20
AkWinchester <arsenijkiselev03@gmail.com>
2023-08-27 12:59:06 +0300

    delete comments

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,16 +1,7 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
-import ProfileReducer from "./ProfileReducer";
-import thunkMiddleware from 'redux-thunk'
 import AuthReducer from "./AuthReducer";
 import {configureStore} from "@reduxjs/toolkit";
 import profileReducer from "./ProfileReducer";
 
-// let rootReducer = combineReducers({
-//     Profile: ProfileReducer,
-//     Auth: AuthReducer
-// })
-
-// let store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
 const store = configureStore({
     reducer:{
